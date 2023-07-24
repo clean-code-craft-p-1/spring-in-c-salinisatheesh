2b3fd17b40f14855c820a3d3192afab6d888c934
salinisatheesh <salini.radhika@philips.com>
2023-07-24 17:19:47 +0530

    changes for test to run

--- stats.cpp
+++ stats.cpp
@@ -1,8 +1,42 @@
 #include "stats.h"
+#include "alerter.h"
+// Define call counters for emailAlerter and ledAlerter
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+struct Stats compute_statistics(const float* numbers, int length) {
+    Stats result;
+
+    if (length == 0) {
+        result.average = 0.0;
+        result.max = 0.0;
+        result.min = 0.0;
+        return result;
+    }
+
+    result.min = numbers[0];
+    result.max = numbers[0];
+    float sum = numbers[0];
+
+    for (int i = 1; i < length; ++i) {
+        if (numbers[i] < result.min) {
+            result.min = numbers[i];
+        }
+        if (numbers[i] > result.max) {
+            result.max = numbers[i];
+        }
+        sum += numbers[i];
+    }
+
+    result.average = sum / static_cast<float>(length);
+
+    return result;
+}
+
+void check_and_alert(const float maxThreshold, alerter_funcptr* alerters, struct Stats computedStats) {
+    if (computedStats.max > maxThreshold) {
+        for (int i = 0; i < 2; ++i) {
+            alerters[i](); 
+        }
+    }
 }
